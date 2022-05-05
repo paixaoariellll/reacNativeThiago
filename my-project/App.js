56de3b0bfb10447690e63cc1b24954692a6ae29b
paixaoariellll <paixaoariellll@gmail.com>
2022-05-05 14:19:03 -0300

    primeira pagina

---
+++ my-project/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+
+const Separator = () => (
+  <View style={styles.separator} />
+);
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <View>
+      <Text style={styles.title}>
+        Pressione o botão abaixo
+      </Text>
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Esse é um exemplo de alerta')}
+      />
+    </View>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
+
+export default App;
